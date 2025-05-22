9e6323a8cab1b942238aefad7fa592e68a71f3c4
David Dudas <david.dudas@outlook.com>
2025-05-22 22:03:15 +0200

    start RViz from world.launch and bridge clock from gz

--- launch/world.launch.py
+++ launch/world.launch.py
@@ -3,14 +3,30 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import  LaunchConfiguration, PathJoinSubstitution, TextSubstitution
-
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, TextSubstitution
+from launch_ros.actions import Node
+from launch.conditions import IfCondition
 
 def generate_launch_description():
+
+    rviz_launch_arg = DeclareLaunchArgument(
+        'rviz', default_value='true',
+        description='Open RViz'
+    )
+
+    rviz_config_arg = DeclareLaunchArgument(
+        'rviz_config', default_value='parametric.rviz',
+        description='RViz config file'
+    )
 
     world_arg = DeclareLaunchArgument(
         'world', default_value='world.sdf',
         description='Name of the Gazebo world file to load'
+    )
+
+    sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time', default_value='True',
+        description='Flag to enable use_sim_time'
     )
 
     pkg_multi_robot_navigation = get_package_share_directory('multi_robot_navigation')
@@ -19,7 +35,8 @@ def generate_launch_description():
     # Add your own gazebo library path here
     gazebo_models_path = "/home/david/gazebo_models"
     os.environ["GZ_SIM_RESOURCE_PATH"] += os.pathsep + gazebo_models_path
-
+    gazebo_models_path, ignore_last_dir = os.path.split(pkg_multi_robot_navigation)
+    os.environ["GZ_SIM_RESOURCE_PATH"] += os.pathsep + gazebo_models_path
 
     gazebo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -35,9 +52,38 @@ def generate_launch_description():
         'on_exit_shutdown': 'true'}.items()
     )
 
+    # Node to bridge messages like /cmd_vel and /odom
+    gz_bridge_node = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+        ],
+        output="screen",
+        parameters=[
+            {'use_sim_time': True},
+        ]
+    )
+
+    # Launch rviz
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', PathJoinSubstitution([pkg_multi_robot_navigation, 'rviz', LaunchConfiguration('rviz_config')])],
+        condition=IfCondition(LaunchConfiguration('rviz')),
+        parameters=[
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
+        ]
+    )
+
     launchDescriptionObject = LaunchDescription()
 
     launchDescriptionObject.add_action(world_arg)
+    launchDescriptionObject.add_action(rviz_launch_arg)
+    launchDescriptionObject.add_action(rviz_config_arg)
+    launchDescriptionObject.add_action(sim_time_arg)
     launchDescriptionObject.add_action(gazebo_launch)
+    launchDescriptionObject.add_action(gz_bridge_node)
+    launchDescriptionObject.add_action(rviz_node)
 
     return launchDescriptionObject
